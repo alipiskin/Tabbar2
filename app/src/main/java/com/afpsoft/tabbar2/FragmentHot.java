227489d83041e7c11d48a49b50f4049506fd719f
alipiskin <alipiskin@hotmail.com>
2018-02-27 08:41:32 +0300

    Initial commit

---
+++ app/src/main/java/com/afpsoft/tabbar2/FragmentHot.java
@@ -0,0 +1,29 @@
+package com.afpsoft.tabbar2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by alipi on 27.02.2018.
+ */
+
+public class FragmentHot extends Fragment {
+
+    TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_hot,container,false);
+        textView= view.findViewById(R.id.textView);
+
+        return view;
+
+    }
+}
